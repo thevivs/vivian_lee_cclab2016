48ca444c7141ebe8f9dd93d8f3fc8955a21c7712
Vivian Lee <leet107@newschool.edu>
2016-10-30 21:30:49 -0400

    week10Homework

---
+++ w08_h08_openFrameworks/src/Circle.hpp
@@ -0,0 +1,14 @@
+//
+//  Circle.hpp
+//  w08_h08_openFrameworks
+//
+//  Created by Vivian Lee on 10/27/16.
+//
+//
+
+#ifndef Circle_hpp
+#define Circle_hpp
+
+#include <stdio.h>
+
+#endif /* Circle_hpp */
